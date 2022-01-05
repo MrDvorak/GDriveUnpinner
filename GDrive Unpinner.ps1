7d8b7e1ca70853d2045d2b594eb105c8d560dc1c
Max Zvyagintsev <max.post.space@gmail.com>
2022-01-05 22:49:15 +0300

    Fix quotes

--- GDrive Unpinner.ps1
+++ GDrive Unpinner.ps1
@@ -6,7 +6,7 @@ $app = New-Object -ComObject shell.application
 
 function Get-GDrive
 {
-    return $app.Namespace("shell:::{679F85CB-0220-4080-B29B-5540CC05AAB6}").Items() | Where-Object { $_.Name -match '^Google Drive \([A-Z]:\)$' }
+    return $app.Namespace("shell:::{679F85CB-0220-4080-B29B-5540CC05AAB6}").Items() | Where-Object { $_.Name -match "^Google Drive \([A-Z]:\)$" }
 }
 
 # Wait for GDrive to Quick access pipping
@@ -15,11 +15,11 @@ while (!$(Get-GDrive))
     # Does we wait too long?
     if ((Get-Date).Second - $start -gt 60*3)
     {
-	Write-Verbose 'GDrive wasn't found!'
+	Write-Verbose "GDrive wasn't found!"
         exit
     }
 
-    Write-Verbose 'Waiting for GDrive...'
+    Write-Verbose "Waiting for GDrive..."
     Start-Sleep -Seconds 3
 }
 
